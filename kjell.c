7e6c58fd1d136cf68034c1a8d0092c3f2028b60c
Samuel Moritz <samuel.moritz@gmail.com>
2014-02-20 19:19:25 +0100

    Working Kjell!

--- kjell.c
+++ kjell.c
@@ -1,67 +1,16 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-#define T 150
-
-o(){
+h(int T){
     digitalWrite (0, HIGH) ; delayMicroseconds (T) ;
-    digitalWrite (0,  LOW) ; delayMicroseconds (3*T) ;
+}
+l(int T){
+    digitalWrite (0, LOW) ; delayMicroseconds (T) ;
 }
 
-x(){
-    digitalWrite (0, HIGH) ; delayMicroseconds (3*T) ;
-    digitalWrite (0,  LOW) ; delayMicroseconds (T) ;
-
-}
-
-stop() {
-    digitalWrite (0, HIGH) ; delayMicroseconds (T) ;
-    digitalWrite (0,  LOW) ; delayMicroseconds (32*T) ;
-
-}
-
-common() {
-	// address, 5 bits, padded with trailing ones = 10 bits
-	x();
-	x();
-	x();
-	x();
-	x();
-	x();
-	x();
-	x();
-	x();
-	x();
-	// button, 5 bits, padded with leading zeroes = 10 bits
-	o();
-	o();
-	o();
-	x();
-	o();
-	x();
-	o();
-	x();
-	o();
-	x();
-	//unknown
-	o();
-	x();
-	//on/off
-
-}
-
-on() {
-	common();
-	o();
-	x();
-	stop();
-}
-
-off() {
-	common();
-	o();
-	o();
-	stop();
+a() {
+	h(450);
+	l(150);
 }
 
 int main ( int argc, char *argv[] )
@@ -69,11 +18,59 @@ int main ( int argc, char *argv[] )
   //printf("The argument supplied is %s\n", argv[1]);
   wiringPiSetup () ;
   pinMode (0, OUTPUT) ;
-  for (;;)
+int i = 0;
+  for (i = 0; i < 8; i++)
   {
-	on();on();on();on(); 
-	delay(1000);
-	off();off();off();off();
-	delay(1000);  
+h(450);
+l(150);
+h(450);
+l(150);
+h(450);
+l(150);
+h(450);
+l(150);
+h(450);
+l(150);
+h(450);
+l(150);
+h(450);
+l(150);
+h(450);
+l(150);
+h(450);
+l(150);
+h(450);
+l(150);
+h(140);
+l(450);
+h(140);
+l(450);
+h(140);
+l(450);
+h(450);
+l(150);
+h(140);
+l(450);
+h(450);
+l(150);
+h(140);
+l(450);
+h(450);
+l(150);
+h(140);
+l(450);
+h(450);
+l(150);
+h(140);
+l(450);
+h(450);
+l(150);
+h(140);
+l(450);
+h(140);
+l(450);
+h(140);
+l(4800);
 	}
+l(0);
 }
