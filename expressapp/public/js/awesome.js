1e374952e0a4be5f89c06fe2111950b2ae4ca1a3
Samuel Moritz <samuel.moritz@gmail.com>
2014-03-18 21:46:36 +0100

    awesome.js

---
+++ expressapp/public/js/awesome.js
@@ -0,0 +1,13 @@
+ $("a").click(function(){
+    if ($(this).hasClass("off")) {
+      $(this).addClass("on");
+      $(this).removeClass("off");
+      $(this).text("On");
+    } else {
+      $(this).addClass("off");
+      $(this).removeClass("on");
+      $(this).text("Off");
+    }
+    $.post('/lampa');
+  });
+
