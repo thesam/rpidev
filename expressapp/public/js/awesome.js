36484cca1795a1b54c38b353a8d48490ec380a90
Manda Nilsson <mandanilsson@Mandas-iMac.local>
2014-03-19 21:02:53 +0100

    Made some changes to the GUI. Added a dropdown menu at the top.

--- expressapp/public/js/awesome.js
+++ expressapp/public/js/awesome.js
@@ -1,12 +1,13 @@
- $("a").click(function(){
+ 
+ $("div.controll").find("a").click(function(){
     if ($(this).hasClass("off")) {
       $(this).addClass("on");
       $(this).removeClass("off");
-      $(this).text("On");
+      //$(this).text("On");
     } else {
       $(this).addClass("off");
       $(this).removeClass("on");
-      $(this).text("Off");
+      //$(this).text("Off");
     }
     $.post('/lampa');
   });
