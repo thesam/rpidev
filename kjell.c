9e5f45049fc4b15be685e55d22536fb948f6e839
Samuel Moritz <samuel.moritz@gmail.com>
2014-03-25 13:25:22 +0100

    Fix indentation

--- kjell.c
+++ kjell.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 h(int T){
-    digitalWrite (0, HIGH) ; delayMicroseconds (T) ;
+	digitalWrite (0, HIGH) ; delayMicroseconds (T) ;
 }
 l(int T){
-    digitalWrite (0, LOW) ; delayMicroseconds (T) ;
+	digitalWrite (0, LOW) ; delayMicroseconds (T) ;
 }
 
 a() {
@@ -15,62 +15,62 @@ a() {
 
 int main ( int argc, char *argv[] )
 {
-  //printf("The argument supplied is %s\n", argv[1]);
-  wiringPiSetup () ;
-  pinMode (0, OUTPUT) ;
-int i = 0;
-  for (i = 0; i < 8; i++)
-  {
-h(450);
-l(150);
-h(450);
-l(150);
-h(450);
-l(150);
-h(450);
-l(150);
-h(450);
-l(150);
-h(450);
-l(150);
-h(450);
-l(150);
-h(450);
-l(150);
-h(450);
-l(150);
-h(450);
-l(150);
-h(140);
-l(450);
-h(140);
-l(450);
-h(140);
-l(450);
-h(450);
-l(150);
-h(140);
-l(450);
-h(450);
-l(150);
-h(140);
-l(450);
-h(450);
-l(150);
-h(140);
-l(450);
-h(450);
-l(150);
-h(140);
-l(450);
-h(450);
-l(150);
-h(140);
-l(450);
-h(140);
-l(450);
-h(140);
-l(4800);
+	//printf("The argument supplied is %s\n", argv[1]);
+	wiringPiSetup () ;
+	pinMode (0, OUTPUT) ;
+	int i = 0;
+	for (i = 0; i < 8; i++)
+	{
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(450);
+		l(150);
+		h(140);
+		l(450);
+		h(140);
+		l(450);
+		h(140);
+		l(450);
+		h(450);
+		l(150);
+		h(140);
+		l(450);
+		h(450);
+		l(150);
+		h(140);
+		l(450);
+		h(450);
+		l(150);
+		h(140);
+		l(450);
+		h(450);
+		l(150);
+		h(140);
+		l(450);
+		h(450);
+		l(150);
+		h(140);
+		l(450);
+		h(140);
+		l(450);
+		h(140);
+		l(4800);
 	}
-l(0);
+	l(0);
 }
