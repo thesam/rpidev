f4e3fe35d6d270c58cff47d6e8fbf3e91843a458
Jonas Älmeblad <almeblad@gmail.com>
2014-05-08 22:00:02 +0200

    Id nr for the controlls are now printed to the div containing the controll. The id is sent back to the server when pressed.

--- expressapp/public/js/awesome.js
+++ expressapp/public/js/awesome.js
@@ -3,25 +3,30 @@ $(document).ready(function () {
         $('#loading').remove();
         console.log(lamps);
         lamps.forEach(function (lamp) {
-            var control = '<div class="controll"> ' +
-                '<div class="button">' +
-                '<a href="#fakelink" class="btn btn-block btn-lg btn-primary on">' + lamp.name + '</a>' +
-                '</div>' +
-                '</div>';
+          var id = lamp._id;
+            if (lamp.type == 'simple') {
+              var control = '<div id='+id+' class="controll"> ' +
+                  '<div class="button">' +
+                  '<a href="#fakelink" class="btn btn-block btn-lg btn-primary on">' + lamp.name + '</a>' +
+                  '</div>' +
+                  '</div>';
+            }
             if (lamp.type === 'dimmer') {
-                control = '<div class="controll">' +
+                control =
+                  '<div id='+id+' class="controll">' +
                     '<p>' + lamp.name + '</p>' +
                     '<div id="slider" class="ui-slider">' +
-                    '<div class="ui-slider-segment"></div>' +
-                    '<div class="ui-slider-segment"></div>' +
-                    '<div class="ui-slider-segment"></div>' +
+                      '<div class="ui-slider-segment"></div>' +
+                      '<div class="ui-slider-segment"></div>' +
+                      '<div class="ui-slider-segment"></div>' +
                     '</div>' +
-                    '</div>';
+                  '</div>';
             }
             $('div.dynamiccontrols').append(control);
         });
-        $("div.controll").find("a").click(function (event) {
-            var btn = $(this);
+        $("div.controll").click(function (event) {
+            var idForControll = $(this).attr('id');
+            var btn = $(this).find('a');
             if (btn.hasClass("off")) {
                 btn.addClass("on");
                 btn.removeClass("off");
@@ -31,7 +36,8 @@ $(document).ready(function () {
                 btn.removeClass("on");
                 //btn.text("Off");
             }
-            $.post('/lampa');
+
+            $.post('/lampa/'+idForControll);
         });
     });
 });
--- expressapp/server.js
+++ expressapp/server.js
@@ -11,21 +11,23 @@ var lampsdb = new Nedb({ filename: 'db/lamps.db', autoload: true });
 // Fyll databasen om den är tom
 lampsdb.find({}, function (err, lamps) {
     if (lamps.length === 0) {
-        lampsdb.insert({ name: 'Vardagsrum', type: 'simple'});
-        lampsdb.insert({ name: 'Fönster, sovrum', type: 'dimmer'});
+        lampsdb.insert({ name: 'Vardagsrum', type: 'simple', state: 'on'});
+        lampsdb.insert({ name: 'Fönster, sovrum', type: 'dimmer', state: '5'});
     }
 });
 
 app.get('/lampa', function (req, res) {
     lampsdb.find({}, function (err, lamps) {
+      console.log(lamps);
         res.send(lamps);
     })
 });
 
-app.post('/lampa', function (req, res) {
-    console.log("POST!");
+app.post('/lampa/:id', function (req, res) {
     var exec = require('child_process').exec;
-    exec("ls -la", console.log);
+    //exec("ls -la", console.log);
+    var id = req.params.id;
+    console.log("id = " + id);
     res.send();
 });
 
