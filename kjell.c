56c4516112f52557da19319ebc11d491791c094d
Samuel Moritz <samuel.moritz@gmail.com>
2014-09-02 20:31:55 +0200

    Extract constants

--- kjell.c
+++ kjell.c
@@ -1,76 +1,85 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-h(int T){
-	digitalWrite (0, HIGH) ; delayMicroseconds (T) ;
-}
-l(int T){
-	digitalWrite (0, LOW) ; delayMicroseconds (T) ;
+#define SHORT 150
+#define LONG (3*SHORT)
+#define VERYLONG (32*SHORT)
+
+void high(int delay){
+	digitalWrite (0, HIGH) ; delayMicroseconds (delay) ;
 }
 
-a() {
-	h(450);
-	l(150);
+void low(int delay){
+	digitalWrite (0, LOW) ; delayMicroseconds (delay) ;
+}
+
+void init() {
+	wiringPiSetup();
+	pinMode(0, OUTPUT);
+}
+
+void cleanup() {
+	low(0);
+}
+
+void lightOn() {
+	int i = 0;
+	for (i = 0; i < 8; i++) {
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(LONG);
+		low(SHORT);
+		high(SHORT);
+		low(LONG);
+		high(SHORT);
+		low(LONG);
+		high(SHORT);
+		low(LONG);
+		high(LONG);
+		low(SHORT);
+		high(SHORT);
+		low(LONG);
+		high(LONG);
+		low(SHORT);
+		high(SHORT);
+		low(LONG);
+		high(LONG);
+		low(SHORT);
+		high(SHORT);
+		low(LONG);
+		high(LONG);
+		low(SHORT);
+		high(SHORT);
+		low(LONG);
+		high(LONG);
+		low(SHORT);
+		high(SHORT);
+		low(LONG);
+		high(SHORT);
+		low(LONG);
+		high(SHORT);
+		low(VERYLONG);
+	}
 }
 
 int main ( int argc, char *argv[] )
 {
-	//printf("The argument supplied is %s\n", argv[1]);
-	wiringPiSetup () ;
-	pinMode (0, OUTPUT) ;
-	int i = 0;
-	for (i = 0; i < 8; i++)
-	{
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(450);
-		l(150);
-		h(140);
-		l(450);
-		h(140);
-		l(450);
-		h(140);
-		l(450);
-		h(450);
-		l(150);
-		h(140);
-		l(450);
-		h(450);
-		l(150);
-		h(140);
-		l(450);
-		h(450);
-		l(150);
-		h(140);
-		l(450);
-		h(450);
-		l(150);
-		h(140);
-		l(450);
-		h(450);
-		l(150);
-		h(140);
-		l(450);
-		h(140);
-		l(450);
-		h(140);
-		l(4800);
-	}
-	l(0);
+    init();
+    cleanup();
 }
